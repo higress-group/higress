881e2c76473f8c997923aebf149d2cca3b3680bd
NekoByte <engineer.jyao@gmail.com>
2026-07-05 20:55:18 -0700

    test: add moonshot e2e test cases for ai-proxy plugin
    Ref: #1717
    Signed-off-by: NekoByte <engineer.jyao@gmail.com>

--- test/e2e/conformance/tests/go-wasm-ai-proxy.go
+++ test/e2e/conformance/tests/go-wasm-ai-proxy.go
@@ -699,6 +699,70 @@ data: [DONE]
 			},
 			{
 				Meta: http.AssertionMeta{
+					TestCaseName:  "moonshot case 1: non-streaming request",
+					CompareTarget: http.CompareTargetResponse,
+				},
+				Request: http.AssertionRequest{
+					ActualRequest: http.Request{
+						Host:        "api.moonshot.cn",
+						Path:        "/v1/chat/completions",
+						Method:      "POST",
+						ContentType: http.ContentTypeApplicationJson,
+						Body:        []byte(`{"model":"gpt-3","messages":[{"role":"user","content":"你好，你是谁？"}],"stream":false}`),
+					},
+				},
+				Response: http.AssertionResponse{
+					ExpectedResponse: http.Response{
+						StatusCode:  200,
+						ContentType: http.ContentTypeApplicationJson,
+						Body:        []byte(`{"id":"chatcmpl-llm-mock","choices":[{"index":0,"message":{"role":"assistant","content":"你好，你是谁？"},"finish_reason":"stop","logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion","usage":{"prompt_tokens":9,"completion_tokens":1,"total_tokens":10}}`),
+					},
+				},
+			},
+			{
+				Meta: http.AssertionMeta{
+					TestCaseName:  "moonshot case 2: streaming request",
+					CompareTarget: http.CompareTargetResponse,
+				},
+				Request: http.AssertionRequest{
+					ActualRequest: http.Request{
+						Host:        "api.moonshot.cn",
+						Path:        "/v1/chat/completions",
+						Method:      "POST",
+						ContentType: http.ContentTypeApplicationJson,
+						Body:        []byte(`{"model":"gpt-3","messages":[{"role":"user","content":"你好，你是谁？"}],"stream":true}`),
+					},
+				},
+				Response: http.AssertionResponse{
+					ExpectedResponse: http.Response{
+						StatusCode:  200,
+						ContentType: http.ContentTypeTextEventStream,
+						Body: []byte(`data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"role":"assistant","content":""},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"你"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"好"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"，"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"你"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"是"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"谁"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{"content":"？"},"finish_reason":null,"logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk"}
+
+data: {"id":"chatcmpl-llm-mock","choices":[{"index":0,"delta":{},"finish_reason":"stop","logprobs":null}],"created":10,"model":"moonshot-v1-8k","object":"chat.completion.chunk","usage":{"prompt_tokens":9,"completion_tokens":1,"total_tokens":10}}
+
+data: [DONE]
+
+`),
+					},
+				},
+			},
+			{
+				Meta: http.AssertionMeta{
 					TestCaseName:  "qwen case 3: non-streaming request",
 					CompareTarget: http.CompareTargetResponse,
 				},
